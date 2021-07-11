3bd22e96624f68495bb9e4e42a45ea95c3bfc992
Erfang Chen <erfangc@gmail.com>
2021-07-11 16:30:25 -0400

    wip

--- src/main/kotlin/com/stockvaluationservice/stockvaluationservice/StockController.kt
+++ src/main/kotlin/com/stockvaluationservice/stockvaluationservice/StockController.kt
@@ -1,12 +1,17 @@
 package com.stockvaluationservice.stockvaluationservice
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class StockController {
+
+    private val log = LoggerFactory.getLogger(StockController::class.java)
+
     @GetMapping("/stock-valuation-service")
     fun getStock(): StockQuote {
+        log.info("Stock quote queried")
         return StockQuote(
             ticker = "AAPL",
             lastPrice = 100.02,
@@ -15,6 +20,7 @@ class StockController {
 
     @GetMapping
     fun get(): String {
+        log.info("--- Health check pinged ---")
         return "Ok"
     }
 }
