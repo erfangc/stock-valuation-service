9bf7dbc6e48748dca5235c2696eac31e44fe5aa2
Erfang Chen <erfangc@gmail.com>
2021-08-05 22:19:43 -0400

    wip

--- src/main/kotlin/com/stockvaluationservice/stockvaluationservice/requestlogging/RequestLoggingFilter.kt
+++ src/main/kotlin/com/stockvaluationservice/stockvaluationservice/requestlogging/RequestLoggingFilter.kt
@@ -15,6 +15,17 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 /**
+ * This filter wraps around the execution of HTTP requests and perform the following tasks:
+ *
+ * 1. Generate the requestId for the current request, which can be accessed from all Spring components such as the
+ * controller advice
+ *
+ * 2. Convert the [ServletRequest] into a [CachedBodyHttpServletRequest] by prefetching all the bytes from the request
+ * body and storing them inside a reusable byte array. This enables us to repeatedly access the request body input stream
+ * without encountering a "stream is closed" error
+ *
+ * 3. Push the request object into a queue to be logged permanently into DynamoDB/Elasticsearch for auditing purposes
+ *
  * TODO
  * 1 - How to serialize properly in DynamoDB?
  * 2 - Role mapping ... ?
@@ -43,6 +54,9 @@ class RequestLoggingFilter(
         )
     }
 
+    /**
+     * TODO move the consumer into their own classes
+     */
     @PostConstruct
     fun init0() {
         executor.submit {
@@ -93,15 +107,29 @@ class RequestLoggingFilter(
         }
     }
 
-    override fun doFilter(
-        request: ServletRequest,
-        response: ServletResponse,
-        chain: FilterChain,
-    ) {
+    /**
+     * Perform the logic of the filter see detail of what [doFilter] does in [RequestLoggingFilter]
+     *
+     * @see [RequestLoggingFilter]
+     * @param request
+     * @param response
+     * @param chain
+     */
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+
+        /*
+        Sets the requestId for the entire request, any Spring Boot service
+        that calls threadLocalRequestId.get() from this point onwards will
+        get the same requestId as is being set here (as long as the caller is from the same thread - which should
+        be the case unless you are using an executor)
+         */
         threadLocalRequestId.set()
         val requestId = threadLocalRequestId.get()
 
         val cachedRequest = CachedBodyHttpServletRequest(request as HttpServletRequest)
+
+        // logging of the request to DynamoDB/Elasticsearch occurs asynchronously to avoid blocking
+        // this HTTP handling thread
         requestQueue.put(RequestQueue(requestId = requestId, request = cachedRequest))
 
         val start = System.currentTimeMillis()
@@ -109,6 +137,8 @@ class RequestLoggingFilter(
         val stop = System.currentTimeMillis()
         val processingTimeMillis = stop - start
 
+        // logging of the response to DynamoDB/Elasticsearch occurs asynchronously to avoid blocking
+        // this HTTP handling thread
         responseQueue.put(
             ResponseQueue(
                 requestId = requestId,
